0918f8939dd33d79989a462b8c5eeb40e567e69b
JpnTr <2011010222034@ogrenci.karabuk.edu.tr>
2019-11-09 05:21:37 +0300

    Update install_dlib.sh

--- install_dlib.sh
+++ install_dlib.sh
@@ -1,6 +1,6 @@
 wget http://dlib.net/files/dlib-19.18.tar.bz2
 tar xvf dlib-19.18.tar.bz2
-cd dlib-19.17/
+cd dlib-19.18/
 sed -i 's,forward_algo = forward_best_algo;,//forward_algo = forward_best_algo;,g' dlib/cuda/cudnn_dlibapi.cpp
 mkdir build
 cd build
