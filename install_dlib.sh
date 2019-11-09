a92c08db5d54847b020cc2f5ba77b4345fd9537b
JpnTr <2011010222034@ogrenci.karabuk.edu.tr>
2019-11-09 05:11:52 +0300

    Create install_dlib.sh

---
+++ install_dlib.sh
@@ -0,0 +1,10 @@
+wget http://dlib.net/files/dlib-19.18.tar.bz2
+tar xvf dlib-19.18.tar.bz2
+cd dlib-19.17/
+sed -i 's,forward_algo = forward_best_algo;,//forward_algo = forward_best_algo;,g' dlib/cuda/cudnn_dlibapi.cpp
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cd ..
+sudo python setup.py install
