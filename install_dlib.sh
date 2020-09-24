6364b3688c7b25d9ed5b926a8a493a4c8f13b161
JpnTr <2011010222034@ogrenci.karabuk.edu.tr>
2020-09-24 23:05:19 +0300

    Update install_dlib.sh
    changed for python 3

--- install_dlib.sh
+++ install_dlib.sh
@@ -1,10 +1,10 @@
-wget http://dlib.net/files/dlib-19.18.tar.bz2
-tar xvf dlib-19.18.tar.bz2
-cd dlib-19.18/
+wget http://dlib.net/files/dlib-19.21.tar.bz2
+tar xvf dlib-19.21.tar.bz2
+cd dlib-19.21/
 sed -i 's,forward_algo = forward_best_algo;,//forward_algo = forward_best_algo;,g' dlib/cuda/cudnn_dlibapi.cpp
 mkdir build
 cd build
-cmake ..
+cmake .. -DDLIB_USE_CUDA=1 -DUSE_AVX_INSTRUCTIONS=1
 cmake --build .
 cd ..
-sudo python setup.py install
+sudo python3 setup.py install
